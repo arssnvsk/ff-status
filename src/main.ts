1c90ef20112e876d246bbdbd808e105a4c42d996
Artem Sosnovsky <arssnvsk@gmail.com>
2025-07-02 12:20:11 +0300

    Revert "init"
    This reverts commit 6612fc4fe981de929225cd8261d193cb71be9ccb.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express';
-import { Handler, Context, Callback } from 'aws-lambda';
-
-let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  server = serverlessExpress({ app: expressApp });
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-export const handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  if (!server) {
-    await bootstrap();
-  }
-  return server(event, context, callback);
-};
+bootstrap();
